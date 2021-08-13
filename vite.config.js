8617582f559c1f5ecdb20c7b98fab31a666c4319
Eric Johnson <ericdj@amazon.com>
2021-08-13 12:09:00 -0600

    set base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/tools/cors/'
 })
